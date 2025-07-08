7e10898f63164ec433a769b59907c67b6d23413e
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2025-07-08 11:22:46 +0800

    Add Main of LumoS

---
+++ LumoS_v0.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+import sys, re
+import numpy as np
+from pprint import pprint
+import csv
+import json
+import random
+from random import sample
+import os
+#from gtts import gTTS
+import pandas as pd
+import time
+from pathlib import Path
+#import nltk
+import re
+#from nltk import sent_tokenize
+#from nltk import tokenize
+#from pyzhuyin import pinyin_to_zhuyin  #, zhuyin_to_pinyin
+
+
+
+if __name__ == "__main__":
+    
+    # Inset ASR package (Open AI Wishper AI)
+    
+    # Text analysis or at least auto-correction
+    
+    # Articut parsing and KeyMoji
+    
+    # Loki analyze the intent and auto-prompt
+    
+    # Prompt ChatGPT to generate the responses
+    
